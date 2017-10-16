fee50934d18a9496f7fd744fdb00506c788dd159
Guillaume <gcerdan83@gmail.com>
2017-10-16 18:05:31 +0200

    index sans php + style

---
+++ login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+      <title>Michel Herbelin | Accès Pro</title>
+      <link href="style.css" rel="stylesheet">
+      <!--<script src="scripts/main.js"></script>-->
+
+</head>
+<body>
+ <div class="form">
+    
+<form class="login-form" name="form1" method="post" action="islogged.php">
+<input name="login" placeholder="Utilisateur" type="text" id="myusername" style="margin-top:30px;">
+<input name="password" type="password" placeholder="Mot de passe" id="mypassword">
+<input type="submit" name="submit" value="Se connecter">
+
+</form>
+</div>
+</body>
+</html>
+
+
